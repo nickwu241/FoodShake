93138e300e3866ae2edce8639aa1d90f8e1e9dc6
craigyu <craig.yu93@gmail.com>
2017-03-18 16:54:56 -0700

    Template for parseYelp

--- app/src/main/java/com/foodshake/ParseYelp.java
+++ app/src/main/java/com/foodshake/ParseYelp.java
@@ -1,13 +1,38 @@
 package com.foodshake;
 
+import com.yelp.clientlib.entities.SearchResponse;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+
 /**
  * Created by chaneric on 2017-03-18.
  */
 
 public class ParseYelp {
+    private HashMap params;
+
+    public void getRest(JSONObject pref){
+        this.params = setPref(pref);
+
+    }
 
 
+    //params.put("term", "food");
+    //params.put("limit", "3");
+    //return:
+    private HashMap setPref(JSONObject inPref){
 
 
+        return null;
+    }
+
+
+    private Call<SearchResponse> getResult(HashMap params) {
+        return null;
+    }
 
 }
