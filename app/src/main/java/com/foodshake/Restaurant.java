c869e763ba9ecbf622977b5b16258837eb0a794f
Eric Chan <ericchan_777@hotmail.com>
2017-03-18 21:29:31 -0700

    Fixed Restaurant being able to access publicly

--- app/src/main/java/com/foodshake/Restaurant.java
+++ app/src/main/java/com/foodshake/Restaurant.java
@@ -15,25 +15,25 @@ import java.util.ArrayList;
 
 public class Restaurant {
 
-    private String id;   // the unique ID of the restaurant
-    private String name; // the name of the restaurant
-    private String imageURL; // the URL for the image associated with the restaurant
-    private String URL;  // the Yelp URL
-    private String price; // How much it cost: "$" means cheap "$$$$" means expensive
-    private double rating;  // how high it is rated, decimals are possible.
-    private int reviewCount; // review count
-    private String phoneNumber; // the phone number for the business
-    private ArrayList<String> arrayOfPhotos; // An array of links to the photos
-    private ArrayList<Hour> hours; // the hours associated with the restaurant.  It is in a JSON Array.
-    private Coordinates coordinates; // gets the Lat/Lon for the restaurant
-    private Location location; // the location of the restaurant.  includes everything relevant to the location
+    public String id;   // the unique ID of the restaurant
+    public String name; // the name of the restaurant
+    public String imageURL; // the URL for the image associated with the restaurant
+    public String URL;  // the Yelp URL
+    public String price; // How much it cost: "$" means cheap "$$$$" means expensive
+    public double rating;  // how high it is rated, decimals are possible.
+    public int reviewCount; // review count
+    public String phoneNumber; // the phone number for the business
+    public ArrayList<String> arrayOfPhotos; // An array of links to the photos
+    public ArrayList<Hour> hours; // the hours associated with the restaurant.  It is in a JSON Array.
+    public Coordinates coordinates; // gets the Lat/Lon for the restaurant
+    public Location location; // the location of the restaurant.  includes everything relevant to the location
 
     public Restaurant(Business businessObject) {
         parseData(businessObject);
     }
 
     private void parseData(Business businessObject) {
-        final String id = businessObject.getId();
+        this.id = businessObject.getId();
         this.name = businessObject.getName();
         this.imageURL = businessObject.getImageUrl();
         this.URL = businessObject.getUrl();
